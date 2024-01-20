e94df968c83f36d32b47f0074c73e045ce36df48
koreaioi <koreaioi@gachon.ac.kr>
2024-01-20 21:11:28 +0900

    HashMap

--- src/ch11/Ex11_17.java
+++ src/ch11/Ex11_17.java
@@ -10,6 +10,11 @@ public class Ex11_17 {
         map.put("강자바", new Integer(80));
         map.put("안자바", new Integer(90));
 
+
+
+
+
+        //key와 value가 한쌍으로 묶인게 entry이고 이를 set으로 저장
         Set set = map.entrySet();
         Iterator it = set.iterator();
 
@@ -18,8 +23,24 @@ public class Ex11_17 {
             System.out.println("이름: " + e.getKey() + ", 점수: " + e.getValue());
         }
 
+        //이름만 보기 위해 key의 집합(set)만 가져옴
         set = map.keySet();
         System.out.println("참가자 명단: " +set);
+
+        //value만 사용하므로 values 사용
+        Collection values = map.values();
+        it = values.iterator();
+        int total =0;
+
+        while (it.hasNext()) total += (int)it.next();
+
+        System.out.println("총점: " + total);
+        System.out.println("평균: " + (float) total / set.size());
+        System.out.println("최고점수: " + Collections.max(values));
+        System.out.println("최저점수: " + Collections.min(values));
+
+
+
 
 
     }
